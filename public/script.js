45595416e61f4e6e4a221500899171eebbdc2e10
Valerka95NeHuck <92526430+Valerka95NeHuck@users.noreply.github.com>
2025-02-25 11:28:51 +0300

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,8 @@
+function setBgGrey (){
+    let myColor = document.getElementById("boxColor");
+    if(myColor.style.background ==='grey'){ 
+        myColor.style.background ='white'
+    } else{
+        myColor.style.background ='grey'
+    }
+}
